1fe8d0797784b2c429aafcd3a12d63cba92854c5
André Sassiente <63066182+AndreSassiente@users.noreply.github.com>
2024-02-22 10:41:11 -0300

    Update ex001.java

--- estrutura-sequencial/ex001.java
+++ estrutura-sequencial/ex001.java
@@ -1,3 +1,5 @@
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.*/
+
 package udemyjava;
 
 import java.util.Scanner;
