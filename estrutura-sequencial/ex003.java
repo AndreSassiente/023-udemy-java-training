7b18a10e999d54ea8d228bebf9dc260054adb6a9
André Sassiente <63066182+AndreSassiente@users.noreply.github.com>
2024-02-22 11:09:06 -0300

    Add files via upload

---
+++ estrutura-sequencial/ex003.java
@@ -0,0 +1,33 @@
+/*
+    Fazer um programa para ler quatro valores inteiros A, B, C e D.
+    A seguir, calcule e mostre a diferença do produto de A e B pelo 
+    produto de C e D segundo a fórmula:
+
+    DIFERENCA = (A*B) - (C*D)
+ */
+package udemy;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Udemy {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, d, diff;
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        
+        diff = (a * b) - (c * d);
+        
+        System.out.println("DIFERENCA = " + diff);
+        sc.close();
+    }
+
+}
